2b6c43583276554c546726bdc51aab27b8d839c9
Maksim Tsoy <>
2023-02-13 21:19:33 +0300

    solution mutex/deadlock

---
+++ mutex/deadlock/sims/one.cpp
@@ -0,0 +1,25 @@
+#include <wheels/test/framework.hpp>
+
+// https://gitlab.com/Lipovsky/tinyfibers
+#include <tf/sched/spawn.hpp>
+#include <tf/sched/yield.hpp>
+#include <tf/sync/mutex.hpp>
+
+using tf::Mutex;
+using tf::Spawn;
+using tf::Yield;
+
+void OneFiberDeadLock() {
+  Mutex mutex;
+
+  auto fiber = [&] {
+    // I am a Fiber
+    mutex.Lock();
+    mutex.Lock();
+  };
+
+  Spawn(fiber).Join();
+
+  // We do not expect to reach this line
+  FAIL_TEST("No deadlock =(");
+}
