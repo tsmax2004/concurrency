7361c62cf0a97b2135f7b27987e6d70354d09c7b
Maksim Tsoy <>
2023-02-13 21:41:44 +0300

    solution for mutex/mutexed

---
+++ mutex/mutexed/mutexed.hpp
@@ -0,0 +1,62 @@
+#pragma once
+
+#include <twist/ed/stdlike/mutex.hpp>
+
+//////////////////////////////////////////////////////////////////////
+
+/*
+ * Safe API for mutual exclusion
+ *
+ * Usage:
+ *
+ * Mutexed<std::vector<Apple>> apples;
+ *
+ * {
+ *   auto owner_ref = apples->Acquire();
+ *   owner_ref->push_back(Apple{});
+ * }  // <- release ownership
+ *
+ */
+
+template <typename T, class Mutex = twist::ed::stdlike::mutex>
+class Mutexed {
+  struct OwnerRef {
+    OwnerRef(T* object, Mutex& mutex)
+        : object_(object),
+          guard_(mutex) {
+    }
+
+    T* operator->() {
+      return object_;
+    }
+    T& operator*() {
+      return *object_;
+    }
+
+   private:
+    T* object_;
+    std::lock_guard<Mutex> guard_;
+  };
+
+ public:
+  // https://eli.thegreenplace.net/2014/perfect-forwarding-and-universal-references-in-c/
+  template <typename... Args>
+  explicit Mutexed(Args&&... args)
+      : object_(std::forward<Args>(args)...) {
+  }
+
+  OwnerRef Acquire() {
+    return OwnerRef(&object_, mutex_);
+  }
+
+ private:
+  T object_;
+  Mutex mutex_;  // Guards access to object_
+};
+
+//////////////////////////////////////////////////////////////////////
+
+template <typename T>
+auto Acquire(Mutexed<T>& object) {
+  return object.Acquire();
+}
