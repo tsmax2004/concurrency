78a0e2b229365e02be3f40dff202a204a1a34828
Maksim Tsoy <>
2023-02-22 10:35:43 +0300

    fix mutex/mutex: no syscalls when no-contention

--- mutex/mutex/mutex.hpp
+++ mutex/mutex/mutex.hpp
@@ -15,22 +15,23 @@ class Mutex {
 
  public:
   void Lock() {
-    auto old_locked = locked_.exchange(State::Locked);
-    while (old_locked == State::Locked) {
+    while (locked_.exchange(State::Locked) == State::Locked) {
+      contention_.fetch_add(1);
       twist::ed::Wait(locked_, State::Locked);
-      old_locked = locked_.exchange(State::Locked);
+      contention_.fetch_sub(1);
     }
   }
 
   void Unlock() {
-    auto wake_key = twist::ed::PrepareWake(locked_);
-
     locked_.store(State::Free);
-    twist::ed::WakeOne(wake_key);
+    if (contention_.load() >= 1) {
+      twist::ed::WakeOne(twist::ed::PrepareWake(locked_));
+    }
   }
 
  private:
   twist::ed::stdlike::atomic<uint32_t> locked_{0};
+  twist::ed::stdlike::atomic<uint32_t> contention_{0};
 };
 
 }  // namespace stdlike
