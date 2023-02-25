1c239e7481084fd45119853fe31647d6bd7fe954
Maksim Tsoy <>
2023-02-25 20:11:39 +0300

    cond/condvar solution

---
+++ cond/condvar/condvar.hpp
@@ -0,0 +1,36 @@
+#pragma once
+
+#include <twist/ed/stdlike/atomic.hpp>
+#include <twist/ed/wait/sys.hpp>
+
+#include <cstdint>
+
+namespace stdlike {
+
+class CondVar {
+ public:
+  // Mutex - BasicLockable
+  // https://en.cppreference.com/w/cpp/named_req/BasicLockable
+  template <class Mutex>
+  void Wait(Mutex& mutex) {
+    auto old = sleep_.fetch_add(1);
+    mutex.unlock();
+    twist::ed::Wait(sleep_, old + 1);
+    mutex.lock();
+  }
+
+  void NotifyOne() {
+    sleep_.fetch_add(1);
+    twist::ed::WakeOne(twist::ed::PrepareWake(sleep_));
+  }
+
+  void NotifyAll() {
+    sleep_.fetch_add(1);
+    twist::ed::WakeAll(twist::ed::PrepareWake(sleep_));
+  }
+
+ private:
+  twist::ed::stdlike::atomic<uint32_t> sleep_{0};
+};
+
+}  // namespace stdlike
---
+++ cond/condvar/mutex.hpp
@@ -0,0 +1,50 @@
+#pragma once
+
+#include <twist/ed/stdlike/atomic.hpp>
+#include <twist/ed/wait/sys.hpp>
+
+#include <cstdint>
+
+namespace stdlike {
+
+class Mutex {
+ public:
+  void Lock() {
+    contention_.fetch_add(1);
+    while (locked_.exchange(State::Locked) == State::Locked) {
+      twist::ed::Wait(locked_, State::Locked);
+    }
+  }
+
+  void Unlock() {
+    contention_.fetch_sub(1);
+    auto key = twist::ed::PrepareWake(locked_);
+
+    locked_.store(State::Free);
+    if (contention_.load() >= 1) {
+      twist::rt::thread::WakeOne(key);
+    }
+  }
+
+  // BasicLockable
+  // https://en.cppreference.com/w/cpp/named_req/BasicLockable
+
+  void lock() {  // NOLINT
+    Lock();
+  }
+
+  void unlock() {  // NOLINT
+    Unlock();
+  }
+
+ private:
+  enum State : uint32_t {
+    Free = 0,
+    Locked = 1,
+  };
+
+  twist::ed::stdlike::atomic<uint32_t> locked_{0};
+  twist::ed::stdlike::atomic<uint32_t> contention_{0};
+};
+
+}  // namespace stdlike
