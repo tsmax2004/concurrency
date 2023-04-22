4cbbe673959071e9f1851f472c2dd8a13ce572a6
Maksim Tsoy <>
2023-04-22 22:31:17 +0300

    downgrade mo

--- lockfree/steal/work_stealing_queue.hpp
+++ lockfree/steal/work_stealing_queue.hpp
@@ -21,9 +21,9 @@ class WorkStealingQueue {
     }
 
     buffer_[tail_ % Capacity].item = item;
-    buffer_[tail_ % Capacity].is_filled.store(true, std::memory_order_release);
+    buffer_[tail_ % Capacity].is_filled.store(true, std::memory_order_relaxed);
 
-    tail_.fetch_add(1, std::memory_order_acquire);
+    tail_.fetch_add(1, std::memory_order_release);
     return true;
   }
 
@@ -39,15 +39,15 @@ class WorkStealingQueue {
 
   // Returns number of tasks
   size_t Grab(std::span<T*> out_buffer) {
-    auto grab_head = head_.load(std::memory_order_acquire);
+    auto grab_head = head_.load(std::memory_order_relaxed);
     auto grab_size = std::min(out_buffer.size(), Size());
     while (!head_.compare_exchange_strong(grab_head, grab_head + grab_size,
-                                          std::memory_order_release,
+                                          std::memory_order_relaxed,
                                           std::memory_order_relaxed)) {
       grab_size = std::min(out_buffer.size(), Size());
     }
     if (!buffer_[grab_head % Capacity].is_filled.load(
-            std::memory_order_acquire)) {
+            std::memory_order_relaxed)) {
       return 0;
     }
 
@@ -58,14 +58,14 @@ class WorkStealingQueue {
  private:
   size_t Size() {
     return tail_.load(std::memory_order_acquire) -
-           head_.load(std::memory_order_acquire);
+           head_.load(std::memory_order_relaxed);
   }
 
   void MoveItems(size_t from, size_t to, std::span<T*> out) {
     for (auto i = from; i < to; ++i) {
       out[i - from] = buffer_[i % Capacity].item;
       buffer_[i % Capacity].item = nullptr;
-      buffer_[i % Capacity].is_filled.store(false, std::memory_order_release);
+      buffer_[i % Capacity].is_filled.store(false, std::memory_order_relaxed);
     }
   }
 
